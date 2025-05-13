4e30c0c0478a1529f7ddca7e513cc0e3e6208266
Bilalskyered <Bilalyoussuf21@gmail.com>
2025-05-13 14:08:24 +0200

    added python certificate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // z.B. 'linux': 'certificates/linux_cert.jpg'
 const skillImageMap = {
   "linux & bash": "./certificates/linux_bash_cert.jpg",
+  "python": "./certificates/python.jpg",
   // Weitere Zuordnungen möglich ...
   // Skills ohne Eintrag erhalten "Zertifikat ist in progress"
 };
